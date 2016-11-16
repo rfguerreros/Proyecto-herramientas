ca0df693265969bc451c27518b6f2e4ec46f45b9
rfguerreros <rfguerreros@unal.edu.co>
2016-11-16 12:07:29 -0500

    Modificacion 1

--- PH-01.cpp
+++ PH-01.cpp
@@ -3,8 +3,13 @@
 #include <fstream>
 #include <cmath>
 
-const double C = 1.0 //micro F/cm*cm 
-  const double gK = 36 // mmho/cm*cm
+const double C = 1.0; //micro F/cm*cm 
+const double gK = 36; // mmho/cm*cm
+const double gNa = 120; // mmho/cm*cm
+const double gl = 0.3; // mmho/cm*cm
+const double VK = 12; // mV
+const double VNa = -115; // mV
+const double Vl = 10.6; // mV
 
 
 double fV(double V, double n, double m, double h, double t);
