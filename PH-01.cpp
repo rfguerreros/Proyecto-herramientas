a8fe97dc8c1479ad8d5d79469bd1f06757317a28
rfguerreros <rfguerreros@unal.edu.co>
2016-11-16 11:54:15 -0500

    Creacion archivo

---
+++ PH-01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+
+//Estructura membrana
+struct Membrane
+{
+  double V, n, m, h;
+};
+
+int main(void)
+{
+  return 0;
+}
