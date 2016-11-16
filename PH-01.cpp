a39ba4c47c9021b96fdd610563b75f7ade8f445f
rfguerreros <rfguerreros@unal.edu.co>
2016-11-16 12:04:21 -0500

    Creacion archivo

--- PH-01.cpp
+++ PH-01.cpp
@@ -3,11 +3,15 @@
 #include <fstream>
 #include <cmath>
 
-//Estructura membrana
-struct Membrane
-{
-  double V, n, m, h;
-};
+const double C = 1.0 //micro F/cm*cm 
+  const double gK = 36 // mmho/cm*cm
+
+
+double fV(double V, double n, double m, double h, double t);
+double fn(double n, double V, double t);
+double fm(double n, double V, double t);
+double fh(double n, double V, double t);
+
 
 int main(void)
 {
